f92b1757550bc361fc74cc344ef0875c6df98435
Aleksey Sinitcyn <sinalekyl@mail.ru>
2023-04-20 10:41:22 +0700

    Т3 итоговая работа, тетрис. Класс Figure, изменен метод shiftRight (перемещение фигуры в право).

--- T3/src/Figure.java
+++ T3/src/Figure.java
@@ -21,8 +21,6 @@ public class Figure {
         return this.фигура;
     }
 
-  
-
 
     void copyArrayMove() {
         for (int x = 0; x < 10; x++) {
@@ -69,31 +67,32 @@ public class Figure {
     boolean shiftRight(char[][] array) {                                // Перемещение фигуры вправо.
         copyArrayMove();
         boolean endShiftRight = false;
-        for (int x = 0; x < 9; x++) {
-            for (int y = 9; y >= 0; y--) {
-                if (((this.фигураДвиж[x][y] == 'X') || (this.фигураДвиж[x + 1][y] == 'X')) && (y == 9))break;
-                if (((this.фигураДвиж[x][y] == 'X') || (this.фигураДвиж[x + 1][y] == 'X')) && (y < 9)) {
-                    if (this.фигураДвиж[x][y] == 'X') {
-                        this.фигураДвиж[x][y + 1] = 'X';
-                        this.фигураДвиж[x][y] = ' ';
-                        endShiftRight = true;
-                    }
-                    if (this.фигураДвиж[x + 1][y] == 'X') {
-                        this.фигураДвиж[x + 1][y + 1] = 'X';
-                        this.фигураДвиж[x + 1][y] = ' ';
-                    }
 
+        for (int line = 0; line < 9; line++) {
+            if (this.фигураДвиж[line][9] == 'X') {
+                endShiftRight = true;
+                break;
+            }
+        }
+
+        if (!endShiftRight) {
+            for (int col = 9; col >= 0; col--) {
+                for (int line = 0; line < 9; line++) {
+                    if (this.фигураДвиж[line][col] == 'X') {
+                        this.фигураДвиж[line][col] = ' ';
+                        this.фигураДвиж[line][col+1] = 'X';
+                    }
                 }
             }
-            if (endShiftRight) break;
+
         }
 
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 10; y++) {
-                if ((фигураДвиж[x][y] == 'X') && (array[x][y] == 'X')) {
+                if ((this.фигураДвиж[x][y] == 'X') && (array[x][y] == 'X')) {
                     return false;
                 }
-                if ((фигураДвиж[x][y] == 'X') && (x == 9)) {
+                if ((this.фигураДвиж[x][y] == 'X') && (x == 9)) {
                     copyArrayYesMove();
                     return false;
                 }
@@ -108,7 +107,7 @@ public class Figure {
         boolean endShiftLeft = false;
         for (int x = 0; x < 9; x++) {
             for (int y = 0; y >= 9; y++) {
-                if (((this.фигураДвиж[x][y] == 'X') || (this.фигураДвиж[x + 1][y] == 'X')) && (y == 0))break;
+                if (((this.фигураДвиж[x][y] == 'X') || (this.фигураДвиж[x + 1][y] == 'X')) && (y == 0)) break;
                 if (((this.фигураДвиж[x][y] == 'X') || (this.фигураДвиж[x + 1][y] == 'X')) && (y > 0)) {
                     if (this.фигураДвиж[x][y] == 'X') {
                         this.фигураДвиж[x][y - 1] = 'X';
