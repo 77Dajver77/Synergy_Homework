005990f4ec7b698a9195d409cda693c2f367b502
Алексей Синицын <sinalekyl.mail.ru>
2023-04-23 21:04:02 +0700

    Добавил 3 практическое задание к уроку №7

--- T3/T3_Y7/src/T3_Y7_Homework_3.java
+++ T3/T3_Y7/src/T3_Y7_Homework_3.java
@@ -1,12 +1,55 @@
 //3.  Возьмите любую программу, которую вы писали до этого.. какая сердцу ближе. Отрефакторите ее (улучшите читабельность кода)
 //    - путем разбиения крупных частей на методы поменьше. Стало лучше? (надеюсь, да)
-//4.  Отрефакторите(улучшите читабельность) кода вашей реализации анализа курса валют.
-//5.  Реализуйте метод, который возвращает случайную цитату Уолтера Уайта.
-//6.  Реализуйте метод, который выводит explanation сегодняшнего снимка дня NASA.
-//7.  Реализуйте метод, который возвращает explanation снимка дня NASA, в качестве параметра принимайте LocalDate - дату, на которую нужно брать снимок.
-//8.  Реализуйте метод, который принимает два LocalDate, и сохраняет все снимки дня NASA в указанный промежуток.
+//       Задание из Т1_У3
+//        double x, y;
+//        Scanner firstNumber = new Scanner(System.in);
+//        System.out.print("Введите первое число ");
+//        x = firstNumber.nextInt();
+//        Scanner secondNumber = new Scanner(System.in);
+//        System.out.print("Введите второе число ");
+//        y = firstNumber.nextInt();
+//        if (x > y ) {
+//        System.out.println(y/x);
+//        }
+//        if (x == y) {
+//        System.out.println("Числа равны");
+//        }
+//        if (x < y) {
+//        System.out.println(x / y);
+//        }
+//
+//        }
 
-
+import java.util.Scanner;
 
 public class T3_Y7_Homework_3 {
+    public static void main(String[] args) {
+        double x = getRequest();
+        double y = getRequest();
+        getResult(x,y);
+    }
+
+    private static void getResult(double x, double y) {
+        if (x > y) {
+        System.out.println(y / x);
+        }
+        if (x == y) {
+        System.out.println("Числа равны");
+        }
+        if (x < y) {
+        System.out.println(x / y);
+        }
+    }
+
+    static double getRequest() {
+        Scanner number = new Scanner(System.in);
+        System.out.print("Введите число ");
+        double x = number.nextInt();
+        return x;
+
+    }
+
+
 }
+
+
